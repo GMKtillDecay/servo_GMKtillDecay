44c9a6ce87c0d778f3e47b70d877f590d3be0c6a
Kevin Harrington <mad.hephaestus@gmail.com>
2015-10-14 10:33:43 -0400

    finished servo

--- servo.groovy
+++ servo.groovy
@@ -1,54 +1,64 @@
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import javafx.scene.paint.Color;
 
-ArrayList<CSG> allCad=new ArrayList<>()
-
-CSG srv = Vitamins.get("smallservo.stl")
-          .movex(20)
-srv.setColor(Color.GREEN)
-allCad.add(srv)
-
-srv = Vitamins.get("smallservo.stl")
-          .movey(-40)
-srv.setColor(Color.GREEN)
-allCad.add(srv)
-
 CSG servoFactory(   
 		double servoThinDimentionThickness,
 		double servoThickDimentionThickness,
+		double servoShaftSideHeight,
 		double outputShaftDimeter,
 		double shaftToShortSideDistance,
 		double shaftToShortSideFlandgeEdge,
 		double tipOfShaftToBottomOfFlange,
 		double flangeThickness,
-		double flangeLongDimention
+		double flangeLongDimention,
+		double bottomOfFlangeToTopOfBody
 		){
-        CSG shaft = new Cylinder(	outputShaftDimeter/2, // Radius at the top
+		CSG shaft = new Cylinder(	outputShaftDimeter/2, // Radius at the top
 				outputShaftDimeter/2, // Radius at the bottom
 				tipOfShaftToBottomOfFlange, // Height
 			         (int)36//resolution
 			         ).toCSG()
 			         
-	CSG flange = new Cube(	servoThinDimentionThickness,// X dimention
+		CSG flange = new Cube(	servoThinDimentionThickness,// X dimention
 			flangeLongDimention,// Y dimention
 			flangeThickness//  Z dimention
 			)
 			.noCenter()
 			.toCSG()
 			.movey(shaftToShortSideFlandgeEdge-flangeLongDimention)
-			.movex(servoThinDimentionThickness/2)
-         return shaft.union(flange)
+			.movex(-servoThinDimentionThickness/2)
+		CSG body = new Cube(	
+			servoThinDimentionThickness,// X dimention
+			servoThickDimentionThickness,// Y dimention
+			servoShaftSideHeight//  Z dimention
+			)
+			.noCenter()
+			.toCSG()
+			.movez(bottomOfFlangeToTopOfBody-servoShaftSideHeight)
+	    		.movex(-servoThinDimentionThickness/2)
+	    		.movey(shaftToShortSideDistance-servoThickDimentionThickness)
+	    	CSG cord = new Cube(
+			9,// x dimention	
+			3,// y dimention
+			200//  Z dimention
+			)
+			.noCenter()
+			.toCSG()
+			.movex(-4.5)
+			.movey(shaftToShortSideDistance-3)
+			.movez(-200)
+          return shaft.union(flange,body,cord)
 }
 
-allCad.add(servoFactory(11.7,//servoThinDimentionThickness
+return servoFactory(11.7,//servoThinDimentionThickness
 			23.6,// servoThickDimentionThickness
+			25.16, // servoShaftSideHeight
 			3.8,// outputShaftDiameter
                         5.13, //shaftToShortSideDistance
-                        10.0,// shaftToShortSideFlandgeEdge
+                       11.0,// shaftToShortSideFlandgeEdge
                         12.9, // tipOfShaftToBottomOfFlange
-                        1.6 ,//flangeThickness
-                        32.3// flangeLongDimention
+                        2.0 ,//flangeThickness
+                        33.3,// flangeLongDimention
+                        10.16//bottomOfFlangeToTopOfBody
                         
-          ))
-
-return allCad
+          )
